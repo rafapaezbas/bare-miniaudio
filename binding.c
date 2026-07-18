ab66ad1832c0fa0510214d0f97afabafbf1160ff
Rafa <rafa@MacBook-Air-de-user.local>
2026-07-18 09:34:28 +0200

    implemented play callback

--- binding.c
+++ binding.c
@@ -16,6 +16,20 @@ bare_miniaudio_init(js_env_t *env, js_callback_info_t *info) {
   return result;
 }
 
+static js_threadsafe_function_t *end_tsfn;
+
+static void
+on_end_audio(void *user_data, ma_sound *snd) {
+  js_call_threadsafe_function(end_tsfn, NULL, js_threadsafe_function_nonblocking);
+}
+
+static void
+on_end_js(js_env_t *env, js_value_t *fn, void *context, void *data) {
+  js_value_t *global;
+  js_get_global(env, &global);
+  js_call_function(env, global, fn, 0, NULL, NULL);
+}
+
 static js_value_t *
 bare_miniaudio_play(js_env_t *env, js_callback_info_t *info) {
   if (sound_loaded) {
@@ -25,13 +39,13 @@ bare_miniaudio_play(js_env_t *env, js_callback_info_t *info) {
 
   int err;
 
-  size_t argc = 1;
-  js_value_t *argv[1];
+  size_t argc = 2;
+  js_value_t *argv[2];
 
   err = js_get_callback_info(env, info, &argc, argv, NULL, NULL);
   assert(err == 0);
 
-  assert(argc == 1);
+  assert(argc == 2);
 
   size_t len;
   err = js_get_value_string_utf8(env, argv[0], NULL, 0, &len);
@@ -52,7 +66,20 @@ bare_miniaudio_play(js_env_t *env, js_callback_info_t *info) {
     return result;
   }
 
+  js_create_threadsafe_function(
+    env,
+    argv[1],
+    0,
+    1,
+    NULL,
+    NULL,
+    NULL,
+    on_end_js,
+    &end_tsfn
+  );
+
   ma_sound_start(&sound);
+  ma_sound_set_end_callback(&sound, on_end_audio, NULL);
   sound_loaded = true;
 
   return result;
@@ -69,6 +96,8 @@ bare_miniaudio_stop(js_env_t *env, js_callback_info_t *info) {
   }
 
   ma_sound_uninit(&sound);
+  js_release_threadsafe_function(end_tsfn, js_threadsafe_function_release);
+  end_tsfn = NULL;
   sound_loaded = false;
 
   return result;
--- example.js
+++ example.js
@@ -1,8 +1,7 @@
 const binding = require('./binding')
 console.log(binding)
-setInterval(() => console.log('beep'), 1000)
+// setInterval(() => console.log('beep'), 1000)
 console.log(binding.init())
-binding.play('/Users/rafa/track.mp3')
+binding.play('/Users/rafa/track.mp3', () => console.log('finished'))
+// binding.stop()
 
-setTimeout(() => binding.stop(), 3000)
-setTimeout(() => binding.play('/Users/rafa/track-b.mp3'), 4000)
